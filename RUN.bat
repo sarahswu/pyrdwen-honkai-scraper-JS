7a9a23938375cef2a2b28d150702c969f011f82d
sarahswu <56704956+sarahswu@users.noreply.github.com>
2024-03-18 22:14:55 -0700

    added bat files
    bat files so its easier for my friends to use if they please

---
+++ RUN.bat
@@ -0,0 +1,6 @@
+cd "C:\Users\Sarah\Documents\Code\pyrdwen-honkai-scraper-JS-main\pyrdwen-honkai-scraper-JS-main"
+cd server
+start node server.js
+cd ..
+cd client
+start npm start
---
+++ SETUP.bat
@@ -0,0 +1,4 @@
+start npm install axios cheerio
+cd "C:\Users\Sarah\Documents\Code\pyrdwen-honkai-scraper-JS-main\pyrdwen-honkai-scraper-JS-main"
+cd client
+npm install
